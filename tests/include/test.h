abd23b43da05c3cfaf6c9f85a2b694ab7c8aff27
残象之形 <976999484@qq.com>
2022-11-10 16:30:16 +0800

    test(test): delete namespace using

--- tests/include/test.h
+++ tests/include/test.h
@@ -4,9 +4,6 @@
 #include <catch2/generators/catch_generators.hpp>
 #include <fmt/core.h>
 
-using namespace std;
-using namespace fmt;
-
 template<typename T>
 constexpr ::std::string_view type() noexcept
 {
