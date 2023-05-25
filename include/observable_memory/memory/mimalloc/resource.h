9902f64179aa4d2b6812b661273e9466031c4d70
Blurring Shadow <976999484@qq.com>
2023-05-25 17:31:48 +0800

    refactor(memory-mimalloc): WIP

--- include/observable_memory/memory/mimalloc/resource.h
+++ include/observable_memory/memory/mimalloc/resource.h
@@ -1,22 +1,70 @@
 #pragma once
 
-#include <unordered_map>
-#include <memory_resource>
+#include <stdsharp/utility/auto_cast.h>
+#include <unordered_set>
 
-#include <boost/container/pmr/resource_adaptor.hpp>
-#include <mimalloc.h>
 #include <mimalloc-new-delete.h>
 
-namespace observable_memory
-{
-    namespace pmr = ::std::pmr;
-    namespace bpmr = ::boost::container::pmr;
-
-    using boost_mem_src = bpmr::memory_resource;
-}
+#include "../../namespace_alias.h"
 
 namespace observable_memory::mimalloc
 {
+    class allocation
+    {
+        std::size_t size_;
+        std::size_t alignment_;
+        void* ptr_;
+
+    public:
+        allocation(const std::size_t bytes, const std::align_val_t alignment, void* const ptr):
+            size_(bytes), alignment_(stdsharp::auto_cast(alignment)), ptr_(ptr)
+        {
+        }
+
+        [[nodiscard]] constexpr void* get() const noexcept { return ptr_; }
+
+        [[nodiscard]] constexpr std::size_t size() const noexcept { return size_; }
+
+        [[nodiscard]] constexpr std::size_t alignment() const noexcept { return alignment_; }
+
+        [[nodiscard]] bool operator==(const allocation& other) const = default;
+
+    protected:
+        [[nodiscard]] constexpr auto& get() noexcept { return ptr_; }
+    };
+
+    class scoped_allocation : public allocation
+    {
+    public:
+        scoped_allocation(
+            const std::size_t bytes,
+            const std::align_val_t alignment,
+            const bool allocate = true
+        ):
+            allocation(
+                bytes,
+                alignment,
+                allocate ? mi_new_aligned(bytes, stdsharp::auto_cast(alignment)) : nullptr
+            )
+        {
+        }
+
+        void allocate()
+        {
+            if(get() == nullptr) { get() = mi_new_aligned(size(), alignment()); }
+        }
+
+        ~scoped_allocation()
+        {
+            if(get() != nullptr) mi_free_size_aligned(get(), size(), alignment());
+        }
+
+        scoped_allocation(const scoped_allocation&) = delete;
+        scoped_allocation(scoped_allocation&&) = default;
+        scoped_allocation& operator=(const scoped_allocation&) = delete;
+        scoped_allocation& operator=(scoped_allocation&&) = default;
+    };
+
     class memory_resource : pmr::memory_resource
     {
     public:
@@ -25,28 +73,21 @@ namespace observable_memory::mimalloc
         memory_resource& operator=(const memory_resource&) = delete;
         memory_resource& operator=(memory_resource&&) = default;
 
-        void release() noexcept
-        {
-            release_impl();
-            allocations_.clear();
-        }
-
-        ~memory_resource() noexcept override { release_impl(); }
+        void release() noexcept { allocations_.clear(); }
 
     protected:
-        void* do_allocate(std::size_t bytes, std::size_t alignment) override
+        void* do_allocate(const std::size_t bytes, const std::size_t alignment) override
         {
-            auto ptr = impl_.allocate(bytes, alignment);
-
-            allocations_.emplace(ptr, alloc_info{bytes, alignment});
-
-            return ptr;
+            return allocations_.emplace(bytes, alignment).first->get();
         }
 
-        void do_deallocate(void* p, std::size_t bytes, std::size_t alignment) override
+        void do_deallocate(
+            void* const p,
+            const std::size_t bytes,
+            const std::size_t alignment
+        ) noexcept override
         {
-            impl().deallocate(p, bytes, alignment);
-            allocations_.erase(p);
+            allocations_.erase(allocation{p, bytes, alignment});
         }
 
         [[nodiscard]] constexpr bool do_is_equal(const pmr::memory_resource& other) //
@@ -56,20 +97,19 @@ namespace observable_memory::mimalloc
         }
 
     private:
-        void release_impl() noexcept
-        {
-            for(const auto [ptr, info] : allocations_) impl_.deallocate(ptr, info.size);
-        }
-
-        mi_stl_allocator<char> impl_;
-
-        struct alloc_info
-        {
-            size_t size;
-            size_t alignment;
-        };
-
-        ::std::unordered_map<void*, alloc_info> allocations_;
+        std::unordered_set<scoped_allocation> allocations_;
     };
+}
 
+namespace std
+{
+    template<>
+    struct hash<::observable_memory::mimalloc::allocation>
+    {
+        [[nodiscard]] constexpr size_t
+            operator()(const ::observable_memory::mimalloc::allocation& alloc) const noexcept
+        {
+            return hash<void*>{}(alloc.get());
+        }
+    };
 }
---
+++ include/observable_memory/namespace_alias.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <memory_resource>
+#include <stdsharp/memory/memory.h>
+
+namespace observable_memory
+{
+    namespace std = ::std;
+    namespace pmr = std::pmr;
+    namespace stdsharp = ::stdsharp;
+}
