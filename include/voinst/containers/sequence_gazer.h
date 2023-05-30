7914a8232e6fd685a26057d7fa24ea1632c8edb5
Blurring Shadow <976999484@qq.com>
2023-05-30 17:32:37 +0800

    feat(sequence-gazer): WIP

--- include/voinst/containers/sequence_gazer.h
+++ include/voinst/containers/sequence_gazer.h
@@ -1,12 +1,36 @@
 #pragma once
 
 #include <stdsharp/containers/concepts.h>
+
 #include "../namespace_alias.h"
 
 namespace voinst
 {
+    template<typename T>
+    struct stargazer;
+
     template<star::sequence_container Container>
-    class sequence_gazer
+    class stargazer<Container>
     {
+        Container container_;
+
+        using iterator = typename Container::iterator;
+        using const_iterator = typename Container::const_iterator;
+
+    public:
+        template<typename... Args>
+            requires std::constructible_from<Container, Args...>
+        constexpr stargazer(Args&&... args) //
+            noexcept(star::nothrow_constructible_from<Container, Args...>):
+            container_(cpp_forward(args)...)
+        {
+        }
+
+        struct event
+        {
+            const_iterator begin;
+            const_iterator end;
+            bool is_new;
+        };
     };
 }
