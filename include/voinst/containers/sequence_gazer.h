e2ad4cbda1bd2ee3b4fe1ee6645d57a5e41445e8
Blurring Shadow <976999484@qq.com>
2023-12-18 09:46:56 +0800

    buid(deps): update registry

--- include/voinst/containers/sequence_gazer.h
+++ include/voinst/containers/sequence_gazer.h
@@ -2,35 +2,22 @@
 
 #include <stdsharp/containers/concepts.h>
 
-#include "../namespace_alias.h"
+#include "../alias.h"
 
 namespace voinst
 {
-    template<typename T>
-    struct stargazer;
 
-    template<star::sequence_container Container>
-    class stargazer<Container>
+    template<typename Iter>
+    struct remove_event
     {
-        Container container_;
+        Iter begin;
+        Iter end;
+    };
 
-        using iterator = typename Container::iterator;
-        using const_iterator = typename Container::const_iterator;
-
-    public:
-        template<typename... Args>
-            requires std::constructible_from<Container, Args...>
-        constexpr stargazer(Args&&... args) //
-            noexcept(star::nothrow_constructible_from<Container, Args...>):
-            container_(cpp_forward(args)...)
-        {
-        }
-
-        struct event
-        {
-            const_iterator begin;
-            const_iterator end;
-            bool is_new;
-        };
+    template<typename Iter>
+    struct update_event
+    {
+        Iter begin;
+        Iter end;
     };
 }
